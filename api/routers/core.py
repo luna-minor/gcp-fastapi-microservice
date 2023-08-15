9105e6930b35a1e5e53b149fd27a4cb46170fdae
jasonlopez01 <jasonlopez01@gmail.com>
2023-08-15 21:19:16 +0000

    apply linting

--- api/routers/core.py
+++ api/routers/core.py
@@ -3,25 +3,27 @@
 import logging
 from typing import Callable
 
+from fastapi import Request, Response
 from fastapi.routing import APIRoute
-from fastapi import Response, Request
 
-       
+
 class LoggingRoute(APIRoute):
     """Log inbound HTTP Request data"""
+
     def get_route_handler(self) -> Callable:
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:
             req_body = await request.body()
 
-            logging.info("{} Request; URL={}; headers={}; body={}".format(
-                request.method, request.base_url, request.headers, req_body
+            logging.info(
+                "{} Request; URL={}; headers={}; body={}".format(
+                    request.method, request.base_url, request.headers, req_body
                 )
             )
 
             response = await original_route_handler(request)
-            
+
             return response
-            
+
         return custom_route_handler
--- tests/unit/test_healthcheck.py
+++ tests/unit/test_healthcheck.py
@@ -15,4 +15,3 @@ def test_healthcheck_endpoint(test_client):
     response = test_client.get("/healthcheck")
     assert response.status_code == 200
     assert response.json() == {"message": "Service is up", "status": "OK"}
-
